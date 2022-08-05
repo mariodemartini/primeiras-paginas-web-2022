6868c18586f51949b6e9aabd6729d980b818d6cb
Mario de Martini <97710585+mariodemartini@users.noreply.github.com>
2022-08-05 08:28:00 -0300

    atualizando

---
+++ main.js
@@ -0,0 +1,10 @@
+const selecionaMenu = document.querySelectorAll('.botao');
+const listaReceitas = document.querySelectorAll('.receita');
+
+function clicaMenu(){
+    for (indice=0; indice<selecionaMenu.length; indice++){
+        selecionaMenu[indice]
+    }
+     
+}
+
