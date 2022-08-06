e0b0ae0f3ab07fa5ce61dcbaee76384ab451c213
Mario de Martini <97710585+mariodemartini@users.noreply.github.com>
2022-08-06 17:10:19 -0300

    tentativa js

--- main.js
+++ main.js
@@ -1,10 +1,33 @@
-const selecionaMenu = document.querySelectorAll('.botao');
-const listaReceitas = document.querySelectorAll('.receita');
+var botao = document.querySelectorAll(".menu ul > li > .botao");
+for (var i = 0; i < botao.length; i++) {
+    botao[i].addEventListener("click", function (e) {
 
-function clicaMenu(){
-    for (indice=0; indice<selecionaMenu.length; indice++){
-        selecionaMenu[indice]
-    }
-     
+        var cards = this.id;
+
+        if(cards == "T"){
+            // document.getElementsByClassName("todos").style.display = "none";
+            document.getElementsByClassName("receita massas").style.display = "block";
+            document.getElementsByClassName("receita salgados").style.display = "block";
+            document.getElementsByClassName("receita sobremesas").style.display = "block";
+        }
+        if(cards == "M"){
+
+            document.getElementsByClassName("receita massas").style.display = "block";
+            document.getElementsByClassName("receita salgados").style.display = "none";
+            document.getElementsByClassName("receita sobremesas").style.display = "none";  
+        }
+        if(cards == "S"){
+
+            document.getElementsByClassName("receita massas").style.display = "none";
+            document.getElementsByClassName("receita salgados").style.display = "block";
+            document.getElementsByClassName("receita sobremesas").style.display = "none";  
+        }
+        if(cards == "D"){
+
+            document.getElementsByClassName("receita massas").style.display = "none";
+            document.getElementsByClassName("receita salgados").style.display = "none";
+            document.getElementsByClassName("receita sobremesas").style.display = "block";                
+        }
+
+    })
 }
-
