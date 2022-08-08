78bda08e1bd2473edcda6d4190254025a1c7a805
Mario de Martini <97710585+mariodemartini@users.noreply.github.com>
2022-08-08 10:01:33 -0300

    tentativa js

--- main.js
+++ main.js
@@ -1,30 +1,76 @@
-let selecionaMassas = document.getElementsByClassName('receita massas');
-let selecionaSalgados = document.getElementsByClassName('receita salgados');
-let selecionaSobremesa = document.getElementsByClassName('receita sobremesa');
+const selecionaMenu = document.querySelectorAll('[data-menu]');
+const selecionaConteudo = document.querySelectorAll('[data-conteudo]')
 
-function mostraTodos(){
-    selecionaMassas.style.display = 'block'
-    selecionaSalgados.style.display = 'block'
-    selecionaSobremesa.style.display = 'block'
+escondeConteudo = () => {
+    selecionaConteudo.forEach(conteudo => conteudo.classList.add('hide'));
 }
 
-function mostraMassas(){
-    selecionaMassas.style.display = 'block'
-    selecionaSalgados.style.display = 'none'
-    selecionaSobremesa.style.display = 'none'
+inativaMenu = () => {
+    selecionaMenu.forEach(menu => menu.classList.remove('ativa'));
 }
 
-function mostraSalgados(){
-    selecionaMassas.style.display = 'none'
-    selecionaSalgados.style.display = 'block'
-    selecionaSobremesa.style.display = 'none'
+ativaConteudo = (valor) => {
+
+    const conteudo = document.querySelector(`[data-conteudo="${valor}"]`);
+
+    if (conteudo == '') {
+        conteudo.classList.remove('hide');
+    }
+    if (conteudo == 'massas') {
+        conteudo.classList.remove('hide');
+    }
+    if (conteudo == 'salgados') {
+        conteudo.classList.remove('hide');
+    }
+    if (conteudo == 'sobremesa') {
+        conteudo.classList.remove('hide');
+    }
+
+    
 }
 
-function mostraSobremesa(){
-    selecionaMassas.style.display = 'none'
-    selecionaSalgados.style.display = 'none'
-    selecionaSobremesa.style.display = 'block'
+ativarMenu = (menu) => {
+    menu.classList.add('ativa');
 }
+
+selecionaMenu.forEach( menu => menu.addEventListener('click', () => {
+    const valor = menu.dataset.menu
+
+    escondeConteudo();
+    inativaMenu();
+    ativaConteudo(valor);
+    ativarMenu(menu);
+}))
+
+
+
+// let selecionaMassas = document.getElementsByClassName('receita massas');
+// let selecionaSalgados = document.getElementsByClassName('receita salgados');
+// let selecionaSobremesa = document.getElementsByClassName('receita sobremesa');
+
+// function mostraTodos(){
+//     selecionaMassas.style.display = 'block'
+//     selecionaSalgados.style.display = 'block'
+//     selecionaSobremesa.style.display = 'block'
+// }
+
+// function mostraMassas(){
+//     selecionaMassas.style.display = 'block'
+//     selecionaSalgados.style.display = 'none'
+//     selecionaSobremesa.style.display = 'none'
+// }
+
+// function mostraSalgados(){
+//     selecionaMassas.style.display = 'none'
+//     selecionaSalgados.style.display = 'block'
+//     selecionaSobremesa.style.display = 'none'
+// }
+
+// function mostraSobremesa(){
+//     selecionaMassas.style.display = 'none'
+//     selecionaSalgados.style.display = 'none'
+//     selecionaSobremesa.style.display = 'block'
+// }
 
 
 // var botao = document.querySelectorAll(".menu ul > li > .botao");
